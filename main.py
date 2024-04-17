72a939389d8b272d12469e646d7d3f2aa5239347
dearkamoliddin <dearkamoliddin@gmail.com>
2024-04-17 19:07:53 +0500

    fixed

---
+++ main.py
@@ -0,0 +1,14 @@
+
+nums = [12, 34, 5, 3, 2, 423, 2, 23, 44]
+
+num_iter = iter(nums)
+summa = 0
+while True:
+    try:
+        num = next(num_iter)
+        if num % 2 == 0:
+            summa += num
+    except StopIteration:
+        break
+
+print("Sum of even numbers:", summa)
---
+++ test.py
@@ -0,0 +1,5 @@
+
+with open('test.txt', 'w') as file:
+    for i in range(1, 101):
+        file.write(str(i) + '\n')
+
