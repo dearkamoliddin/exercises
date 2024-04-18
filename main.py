31f576a94599c7f7ef0d43d41a1d8414b51f7703
dearkamoliddin <dearkamoliddin@gmail.com>
2024-04-18 11:07:06 +0500

    smthing

--- main.py
+++ main.py
@@ -15,15 +15,16 @@ while True:
 print("Sum of even numbers:", summa)
 
 
-
 def fibonacci_numbers(nums):
     x, y = 0, 1
     for _ in range(nums):
         x, y = y, x+y
         yield x
 
+
 def square(nums):
     for num in nums:
         yield num**2
+
 
 print(sum(square(fibonacci_numbers(10))))
