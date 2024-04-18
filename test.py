de419cc16a72a0b5c10b1aeaf4e71680a8d99bbb
dearkamoliddin <dearkamoliddin@gmail.com>
2024-04-18 11:24:20 +0500

    accurated

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-print("Write number from 1 to 100 to txt file")
+
 with open('test.txt', 'w') as file:
+    """Write number from 1 to 100 to txt file"""
     for i in range(1, 101):
         file.write(str(i) + '\n')
 
